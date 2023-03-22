ab292796319da0d3306f18594e518cebeef3c5a2
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-03-22 12:18:11 +0200

    initial

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,45 @@
-import React,{ FC }  from 'react';
-import { GlobalStyle } from '../GlobalStyle';
+import React, { FC } from "react";
+// import { GlobalStyle } from "../GlobalStyle";
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./../../ColorModeSwitcher";
+import { Logo } from "./../../Logo";
 
-
-const App:FC = () => {
+const App: FC = () => {
   return (
-    <div className="app">
-      <GlobalStyle />
-    </div>
+    // <div className="app">
+    //   <GlobalStyle />
+    // </div>
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            </Text>
+            <Link
+              color="teal.500"
+              href="https://chakra-ui.com"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Chakra
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
   );
-}
+};
 export default App;
